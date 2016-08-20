4789611c369c53b8b66ba6c80c3059cc8fa7f018
chrispicato <christopherpicato@gmail.com>
2016-08-19 19:26:18 -0700

    Implement schema for database

--- server/schema.sql
+++ server/schema.sql
@@ -7,7 +7,99 @@ CREATE TABLE messages (
 );
 
 /* Create other tables and define schemas for them here! */
+-- ---
+-- Globals
+-- ---
 
+-- SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+-- SET FOREIGN_KEY_CHECKS=0;
+
+-- ---
+-- Table 'Users'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `Users`;
+    
+CREATE TABLE `Users` (
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `username` VARCHAR NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Table 'Messages'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `Messages`;
+    
+CREATE TABLE `Messages` (
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `text` VARCHAR NULL DEFAULT NULL,
+  `createdAt` DATE NULL DEFAULT NULL,
+  `userID` INTEGER NULL DEFAULT NULL,
+  `roomID` INTEGER NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Table 'Rooms'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `Rooms`;
+    
+CREATE TABLE `Rooms` (
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `roomname` VARCHAR NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Table 'Friends'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `Friends`;
+    
+CREATE TABLE `Friends` (
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `userID` INTEGER NULL DEFAULT NULL,
+  `friendID` INTEGER NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Foreign Keys 
+-- ---
+
+ALTER TABLE `Messages` ADD FOREIGN KEY (userID) REFERENCES `Users` (`id`);
+ALTER TABLE `Messages` ADD FOREIGN KEY (roomID) REFERENCES `Rooms` (`id`);
+ALTER TABLE `Friends` ADD FOREIGN KEY (userID) REFERENCES `Users` (`id`);
+ALTER TABLE `Friends` ADD FOREIGN KEY (friendID) REFERENCES `Users` (`id`);
+
+-- ---
+-- Table Properties
+-- ---
+
+-- ALTER TABLE `Users` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `Messages` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `Rooms` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `Friends` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ---
+-- Test Data
+-- ---
+
+-- INSERT INTO `Users` (`id`,`username`) VALUES
+-- ('','');
+-- INSERT INTO `Messages` (`id`,`text`,`createdAt`,`userID`,`roomID`) VALUES
+-- ('','','','','');
+-- INSERT INTO `Rooms` (`id`,`roomname`) VALUES
+-- ('','');
+-- INSERT INTO `Friends` (`id`,`userID`,`friendID`) VALUES
+-- ('','','');
 
 
 
